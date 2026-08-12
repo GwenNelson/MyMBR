1c45e85a5a70288c0d29fd7347223bd717a10d77
Gwen Nelson <gwen@gwennelson.co.uk>
2026-08-12 02:09:05 +0100

    evil vibe-coded gen-img that does mkfs

--- gen-img.sh
+++ gen-img.sh
@@ -1,17 +1,43 @@
 #!/bin/bash
+set -e
 
-# Create 32MB disk image (4 partitions × 8MB)
-dd if=/dev/zero of=mbr-test.img bs=1M count=36
+IMAGE=mbr-test.img
+LOOP=""
+
+cleanup()
+{
+    if [ -n "$LOOP" ]; then
+        sudo losetup -d "$LOOP"
+    fi
+}
+
+trap cleanup EXIT
+
+# Create 36MB disk image (4 partitions × 8MB, plus alignment/slack)
+dd if=/dev/zero of="$IMAGE" bs=1M count=36
 
 # Create MBR partition table
-parted -s mbr-test.img mklabel msdos
+parted -s "$IMAGE" mklabel msdos
 
 # Create 4 primary partitions, each 8MB, suitably weird
-parted -s mbr-test.img mkpart primary fat32 1MiB 9MiB
-parted -s mbr-test.img mkpart primary ext2 9MiB 17MiB
-parted -s mbr-test.img mkpart primary ntfs 17MiB 25MiB
-parted -s mbr-test.img mkpart primary ext2 25MiB 33MiB
+parted -s "$IMAGE" mkpart primary fat32 1MiB 9MiB
+parted -s "$IMAGE" mkpart primary ext2 9MiB 17MiB
+parted -s "$IMAGE" mkpart primary ntfs 17MiB 25MiB
+parted -s "$IMAGE" mkpart primary ext2 25MiB 33MiB
 
-# Verify
-parted -l mbr-test.img
+# Attach image and have the kernel expose its partitions
+LOOP=$(sudo losetup --find --show --partscan "$IMAGE")
 
+echo "Image attached as $LOOP"
+
+# Create the filesystems
+sudo mkfs.vfat -F 32 "${LOOP}p1"
+sudo mkfs.ext2 -F "${LOOP}p2"
+sudo mkfs.ntfs -F "${LOOP}p3"
+sudo mkfs.ext2 -F "${LOOP}p4"
+
+# Show what we've made
+sudo fdisk -l "$LOOP"
+sudo blkid "${LOOP}p1" "${LOOP}p2" "${LOOP}p3" "${LOOP}p4"
+
+# cleanup trap detaches LOOP
